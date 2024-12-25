fa298de58066dfe5441b824e377d15f61f7a8d8b
Mihajlo-Spasic <mihajlospasicbusiness@gmail.com>
2024-12-25 16:28:33 +0100

    follow-up of sigleton browser

--- src/main/java/com/home_server/artifact_home_server/ArtifactHomeServerApplication.java
+++ src/main/java/com/home_server/artifact_home_server/ArtifactHomeServerApplication.java
@@ -3,11 +3,20 @@ package com.home_server.artifact_home_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.openqa.selenium.WebDriver;
+import com.home_server.artifact_home_server.listinghandling.Browser_instance;
+import com.home_server.artifact_home_server.database.Database_instance;
+import java.sql.*;
+
 @SpringBootApplication
 public class ArtifactHomeServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException{
 		SpringApplication.run(ArtifactHomeServerApplication.class, args);
+    
+    // Loading all the heavy code at the start 
+    Database_instance database = Database_instance.getInstance(); 
+    WebDriver browser = Browser_instance.getInstance();
 	}
 
 }
--- src/main/java/com/home_server/artifact_home_server/listinghandling/Browser_instance.java
+++ src/main/java/com/home_server/artifact_home_server/listinghandling/Browser_instance.java
@@ -9,20 +9,29 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.facebook_auth_selenium.Facebook_auth;
 
+
+// Browser Instance for KUPUJEMPRODAJEM listings
+// Singleton pattern 
 public class Browser_instance{ 
 
   public static WebDriver driver;
   public static FirefoxOptions options;
   
-  public static String browser = "firefox";
-  public static String driver_path = "/usr/bin/geckodriver";
+  public final static String browser = "firefox";
+  public final static String driver_path = "/usr/bin/geckodriver";
+  public final static String listing_url ="https://www.kupujemprodajem.com/moj-kp/moji-oglasi";
 
+
+  private static boolean set_gecko_location = false;
   private static Browser_instance instance;
 
-  private Browser_instance(){    
+  private Browser_instance(){   
+    if (!set_gecko_location){
+      System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+      set_gecko_location = true;
+    }
     this.options = new FirefoxOptions();
-    this.driver = new FirefoxDriver(options);
-  
+    this.driver = new FirefoxDriver(options); 
   }
 
   public static WebDriver getInstance(){
@@ -36,6 +45,14 @@ public class Browser_instance{
     driver.close();
     instance = null;
   }
-    
   
+  //Method checks if user is still logged to the platform kupujemprodajem
+  public static void checkConnectability(){} 
+  
+  //Set of methods for CRUD of listings managed by database
+  public static void updateListing(){}
+  public static void addListingFromDB(){}
+  public static void removeListingFromDB(String name){}
+  public static void removeListingFromDB(int id){}
 }
+
